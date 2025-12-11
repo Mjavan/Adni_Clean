cbe9a56e78da2669c94ae29b9b6f07a14788c657
masoumeh javanbakhat <masoumeh.javanbakhat@hpc.dhclab.i.hpi.de>
2025-12-11 09:26:24 +0100

    data.py was updated (new stratification was added)

--- src/data.py
+++ src/data.py
@@ -43,3 +43,67 @@ class AdniMRIDataset2D(Dataset):
             img = img[:, :, np.newaxis]  # HW -> HWC
         img = img.transpose(2, 0, 1)  # HWC -> CHW
         return img
+    
+
+## Stratifyng by severity levels
+class AdniMRIDatasetFull(Dataset):
+    def __init__(self, annotations_file, img_dir=None, transform=None):
+        df = pd.read_csv(annotations_file)
+        self.img_labels = df
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        print(f'idx:{idx}')
+        rel_path = self.img_labels["filepath_MNIlin"].iloc[idx]
+        group = self.img_labels["Group"].iloc[idx]   # <-- CN, MCI, AD
+
+        if self.img_dir:
+            img_path = os.path.join(self.img_dir, rel_path)
+        else:
+            img_path = rel_path
+
+        image = nib.load(img_path).get_fdata().astype(np.uint8)
+        image = image.transpose(2, 0, 1)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, group
+    
+
+
+def save_severity_groups(dataset, out_dir="../adni_results/images"):
+    os.makedirs(out_dir, exist_ok=True)
+
+    CN, MCI, AD = [], [], []
+
+    print(f'directory for saving: {out_dir}')
+
+    for img, label in dataset:
+        if label == "CN":
+            CN.append(img)
+        elif label == "MCI":
+            MCI.append(img)
+        elif label == "AD":
+            AD.append(img)
+
+    if CN:
+        np.save(os.path.join(out_dir, "CN.npy"), np.stack(CN))
+        print("Saved CN.npy:", len(CN))
+    if MCI:
+        np.save(os.path.join(out_dir, "MCI.npy"), np.stack(MCI))
+        print("Saved MCI.npy:", len(MCI))
+    if AD:
+        np.save(os.path.join(out_dir, "AD.npy"), np.stack(AD))
+        print("Saved AD.npy:", len(AD))
+
+if __name__ == "__main__":
+    annotations_file = "./file_local.csv"
+    dataset = AdniMRIDatasetFull(annotations_file)
+    out_dir = "/sc/home/masoumeh.javanbakhat/netstore-old/Baysian/3D/Explainability/AdniGithub/adni_results/images"
+    save_severity_groups(dataset)
+
